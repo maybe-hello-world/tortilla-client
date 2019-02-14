bfc23cbd2b1c8c38cbe9a7dd542ffb9631240dde
Kell <maybe.hello.world@gmail.com>
2019-02-14 22:32:37 +0300

    use base64 for userkey & serverkey

--- src/main/java/kell/guacamole/small/domain/UserInfoRestModel.java
+++ src/main/java/kell/guacamole/small/domain/UserInfoRestModel.java
@@ -2,6 +2,10 @@ package kell.guacamole.small.domain;
 
 import com.google.gson.annotations.Expose;
 import lombok.Data;
+import lombok.SneakyThrows;
+import org.apache.guacamole.GuacamoleException;
+
+import java.util.Base64;
 
 @Data
 public class UserInfoRestModel {
@@ -14,13 +18,20 @@ public class UserInfoRestModel {
     @Expose(deserialize = false, serialize = false)
     String password;
 
+    //TODO: check it, is it needed
+    @SneakyThrows(GuacamoleException.class)
     public String getPassword() {
         if (password == null && serverkey != null && userkey != null) {
-            byte[] txt = userkey.getBytes();
-            byte[] key = serverkey.getBytes();
+            byte[] txt = Base64.getDecoder().decode(userkey);
+            byte[] key = Base64.getDecoder().decode(serverkey);
+            if (txt.length != key.length) {
+                throw new GuacamoleException("Keys length mismatch during password reconstruction." +
+                        String.format(" Userkey length: %d, serverkey length: %d.", userkey.length(), serverkey.length()));
+            }
+
             byte[] res = new byte[userkey.length()];
             for (int i = 0; i < txt.length; i++) {
-                res[i] = (byte) (txt[i] ^ key[i % key.length]);
+                res[i] = (byte) (txt[i] ^ key[i]);
             }
             password = new String(res);
         }
